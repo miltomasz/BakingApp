42ed9f0419373d8c063a8b83d7d734fed9e85b87
Tomasz Milczarek <tomasz.milczarek@gmail.com>
2018-04-22 20:06:45 +0200

    Add grid layout for tablet

--- app/src/main/java/com/plumya/bakingapp/ui/list/MainActivity.java
+++ app/src/main/java/com/plumya/bakingapp/ui/list/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity implements RecipesAdapter.Re
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     private static final int CACHE_SIZE = 20;
+    public static final int GRID_COLUMN_COUNT = 3;
 
     private MainActivityViewModel viewModel;
     private RecipesAdapter recipesAdapter;
@@ -46,6 +48,7 @@ public class MainActivity extends AppCompatActivity implements RecipesAdapter.Re
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        initializeLayoutManager();
         initializeRecyclerView();
         showProgressBar(true);
 
@@ -67,6 +70,15 @@ public class MainActivity extends AppCompatActivity implements RecipesAdapter.Re
         });
     }
 
+    private void initializeLayoutManager() {
+        boolean isTablet = getResources().getBoolean(R.bool.tablet);
+        if (isTablet) {
+            recipesRv.setLayoutManager(new GridLayoutManager(this, GRID_COLUMN_COUNT));
+        } else {
+            recipesRv.setLayoutManager(new LinearLayoutManager(this));
+        }
+    }
+
     private void showProgressBar(boolean show) {
         progressBar.setVisibility(show ? View.VISIBLE : View.GONE);
     }
@@ -82,7 +94,6 @@ public class MainActivity extends AppCompatActivity implements RecipesAdapter.Re
     }
 
     private void initializeRecyclerView() {
-        recipesRv.setLayoutManager(new LinearLayoutManager(this));
         recipesRv.setHasFixedSize(true);
         recipesRv.setItemViewCacheSize(CACHE_SIZE);
         recipesRv.setDrawingCacheEnabled(true);
